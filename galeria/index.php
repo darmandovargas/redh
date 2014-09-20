ea88df5fbe03a435540ee777ff3cc99f71ff70a0
Diego Armando Vargas <darmandovargas@gmail.com>
2014-09-19 21:40:05 -0500

    Get rid of php header

--- galeria/index.php
+++ galeria/index.php
@@ -1,10 +1,3 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
