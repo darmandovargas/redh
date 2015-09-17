697efaaeb396ffb413ba56981253ee0d2b160c68
Diego Armando Vargas <darmandovargas@gmail.com>
2015-09-16 20:55:19 -0500

    Agrego html con mensaje de mantenimiento para comportamientos inesperados o en caso que se necesite poner en éste modo.

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 header( 'Location: home' ) ;
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Estamos mejorando para ud</title>
+</head>
+
+<body>
+<img src="home/img/web_maintainance_img.png">
+</body>
+
+</html>
