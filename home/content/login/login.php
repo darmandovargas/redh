bdd328a5cb8a71524ce164a8c70ddbe6b5b7f7c2
Diego Armando Vargas <darmandovargas@gmail.com>
2015-09-16 20:51:18 -0500

    Change admin password.

--- home/content/login/login.php
+++ home/content/login/login.php
@@ -7,7 +7,7 @@
 		$_SESSION['sessid'] = session_id();
 		$_SESSION['username'] = $username;
 		echo "success";
-	}else if(isset($username) && isset($password) && $username == 'admin' && $password == 'admin1234'){
+	}else if(isset($username) && isset($password) && $username == 'admin' && $password == 'UTPAdm!n'){
 		session_start();
 		$_SESSION['sessid'] = session_id();
 		$_SESSION['username'] = $username;
