9e578bb21783f68f2f607a82c34587efdd0a8814
Diego Armando Vargas <darmandovargas@gmail.com>
2018-10-18 21:38:00 -0500

    Alarmas de Nivel con Alertas Amarilla, Naranja y Roja
    * Se ha actualizado el script de alarmas con el fin de permitir alarmas de nivel de tres tipos, amarilla, naranja y roja, las cuales se pasan como alarmas independientes en el json cada una con límites mayor y menor que excepto la roja la cual solo tiene mayor que.

--- lib/alarm.php
+++ lib/alarm.php
@@ -3,11 +3,11 @@
 	 * This function will build the SMS to be sent to the system operator(s)
 	 *
 	 */
-	function checkAlarms (&$oMySQL, $tabla, $stationName, $variable, $lessThan='', $moreThan='', $station){
-		global $Message, $currentLevelValue, $objTemp, $db_aguasName, $bd_aguasUser, $bd_aguasPassword, $bd_aguasIp, $dbsigName, $bdsigUser, $bdsigPassword, $bdsigIp, $updatedIds;
-
-		$bd_aguas = array("rio_otun_eljordan", "rio_azul_eljordan", "rio_barbo_pezfresco", "q_volcanes");
-		
+	function checkAlarms (&$oMySQL, $tabla, $stationName, $variable, $lessThan='', $moreThan='', $stationId){
+		global $Message, $objTemp, $db_aguasName, $bd_aguasUser, $bd_aguasPassword, $bd_aguasIp, $dbsigName, $bdsigUser, $bdsigPassword, $bdsigIp, $updatedIds;
+		// Create a list of the tables that are on the second database
+		$bd_aguas = array("tst_san_juan", "tst_rio_azul_eljordan", "tst_rio_otun_eljordan", "tst_rio_barbo_pezfresco", "tst_q_volcanes");
+		// If the current alarm we are checking is on the array then the conexion must be on the second database, otherway use the first one, the UTP
 		if(in_array($tabla, $bd_aguas)){
 			$oMySQL->closeConnection();
 			unset($oMySQL);
@@ -17,7 +17,6 @@
 			unset($oMySQL);
 			$oMySQL = new MySQL($dbsigName, $bdsigUser, $bdsigPassword, $bdsigIp);
 		}
-
 		// Get current time less than two hours in my timezone
 		$dt = new DateTime('', new DateTimeZone('America/Bogota'));
 		$dt->sub(new DateInterval('PT7200S'));
@@ -25,86 +24,75 @@
 		// convert two hours ago in linux time
 		$twoHoursAgo = strtotime($twoHoursAgo);
 		// Prepare query with last 2 registers from db
-		$query = "SELECT * FROM " . $tabla . " ORDER BY fecha DESC LIMIT 2";
+		$query = "SELECT * FROM ".$tabla." ORDER BY stationTime DESC LIMIT 2";
 		// Run query
 		$rs = $oMySQL->executeSQL($query);
-		// Initialize an average temporal variable
-		$averageTemp = false;
+		
+		//print_r($rs);
+		//echo "</br>---------------------------</br>";
+		// Initialize some variables
 		$vals = array();
-		$averageCount = 0;
 		$response = false;
-
+		// This is for the specific case of river flow where we need three different types of alarms, yellow, orange and red (f.i: riverFlow-yellow)
+		$variableTmp = explode("-", $variable);
+		$variable = $variableTmp[0];
+		$alarmType = isset($variableTmp[1]) ? $variableTmp[1] : "";
+		
+		// If there are results on the query then iterate it
 		if($rs){
-			// Iterates the last 2 registers in order to get the sum of both vallues
+			// Iterates the last 2 registers in order to get the sum of both values
 			foreach ($rs as $v) {
 				// Obtengo la fecha
-				$momento = $v['fecha'].' '.$v['hora'];
+				$momento = $v['statiomTime'];
 				// La pongo en mi zona horaria
 				$dateColombia = new DateTime($momento, new DateTimeZone('America/Bogota'));
-				// La paso a linux para validarla en el siguiente if de manera que la medición que se está iterando sea de la últimas 2 hora
+				// La paso a linux para validarla en el siguiente if de manera que la medición que se está iterando sea de las últimas 2 horas
 				$linuxTimeMedicion = strtotime($dateColombia->format('Y-m-d H:i:sP'));
-				// if the value is not empty and different to 0 it will sum the value
+				// if the value is not empty and different to 0 it will sum the value				
 				if(!empty($v[$variable]) && $linuxTimeMedicion > $twoHoursAgo && strcmp ( $v[$variable] , "-" ) !== 0){ /*floatval($v[$variable]) != 0 &&*/ 
-					$averageTemp += floatval($v[$variable]);
-					$vals[] = floatval($v[$variable]);
-					$averageCount++;	
+					$vals[] = floatval($v[$variable]);						
 				}
 			}
 		}		
-
-		// If value is with the initialized value (false) then do nothing (continue ), OR if the count its different than 2 values, then also continue
-		if(!$averageTemp || count($vals) != 2){
+		// If the count on values taken from database are different than 2, then continue with the cron stations iteration
+		if(count($vals) != 2){
 			return false;
 		}
+		// Initialize last and second to last variables for the rest of the function
+		$currentValue = $vals[0];
+		$penultimateValue = $vals[1];
 
 		// We avoid sms if the last register is lower than second to last register AND
-		// if it's "nivel" we need to take into account last alarm value and the slope, so it will 
+		// if it's "riverFlow" we need to take into account last alarm value and the slope, so it will 
 		// send an alert only if the slope is positive and current value is higher than the last alarm value,
-		// if the current value is lower than last alarm value then last alarm  value will be restted
+		// if the current value is lower than last alarm value then last alarm value will be resetted with zero
 		$activeMoreThan = false;
-		
-		$lastValue = isset($objTemp["last_value"])?$objTemp["last_value"]:0;
-
-		$firstCicleAlarm = isset($objTemp["first_cicle_alarm"])?$objTemp["first_cicle_alarm"]:0;
-
-		if($variable == "nivel" && intval($lastValue) > 0 && intval($firstCicleAlarm) > 0){
-			
-			if( $vals[0] < floatval($moreThan) ){
-				$updatedIds[] = $station;
+		// Set last value registered on the alarm json, if it does not exist initialize it with zero.
+		$lastValue = isset($objTemp["last_value"]) ? $objTemp["last_value"] : 0;
+		// Set 
+		$firstCicleAlarm = isset($objTemp["first_cicle_alarm"]) ? $objTemp["first_cicle_alarm"] : 0;
+		// If we are evaluating the rierFlow, and last value and first cicle alarm are more than zero 
+		if($variable == "riverFlow" && intval($lastValue) > 0 && intval($firstCicleAlarm) > 0){
+			// If the last value is below the alarm value then reset the variables
+			if( $currentValue < floatval($moreThan) ){				
 				$objTemp["last_value"] = 0;	
 				$objTemp["first_cicle_alarm"] = 0;	
-			}else if($vals[0] > floatval($lastValue)){
-				$updatedIds[] = $station;
-				$objTemp["last_value"] = $vals[0];	
+			}else if($currentValue > floatval($lastValue)){
+				// If the last value in database is greater than the last value registered on the last alarm
+				$objTemp["last_value"] = $currentValue;	
 			}
-			//echo $tabla." - ".$variable." - ".$moreThan." - ".$objTemp["last_value"]."</br>";
-			$currentLevelValue = $vals[0];
-
-			if( $vals[0] > floatval($lastValue) && $vals[0] > floatval($moreThan) && $vals[1] > floatval($moreThan)){
-
-				if($vals[0]<=$val[1]){
+			// Add the id to the updateIds array in order to override json value for the current alarm
+			$updatedIds[] = $stationId;
+			// If the last value is greater than last value registered in the alarm json and is greater than the alarm value and the second to last value is graeter than the alarm value
+			if( $currentValue > floatval($lastValue) && $currentValue > floatval($moreThan) && $penultimateValue > floatval($moreThan)){
+				// if last value is below the second to last value it means the river flow is decreasing so avoid the alarm
+				if($currentValue <= $penultimateValue){
 					return false;
 				}else{
-					// First depreciated solution
-					// Check slope of the current measure, if its positive then send the alert, if its negative return false
-					// set variables for slope equation
-					/*$x1 = 1;
-					$x2 = 2;
-					$y1 = $vals[1];
-					$y2 = $vals[0];
-					// get the slope
-					$m = ($y1-$y2)/($x1-$x2);
-					if($m>0){
-						// Possitive slope
-						$activeMoreThan = true;
-					}else{
-						//echo "NEGATIVE SLOPE".$tabla." - ".$variable." - ".$moreThan." - ".$objTemp["last_value"]."</br>";
-						// Negative or null slope
-						return false;
-					}*/
-					//$vals[0]
 					// Second solution, first cicle alarm value and last value
-					if($vals[0] > floatval($lastValue)){
+					// If the last value is over the last registered value on the alarm json, 
+					// set $activeMoreThan as true, otherway avoid the alarm
+					if($currentValue > floatval($lastValue)){
 						// Possitive slope
 						$activeMoreThan = true;
 					}else{
@@ -112,13 +100,16 @@
 					}
 				}
 			}else{
-				//echo "ELSE".$tabla." - ".$variable." - ".$moreThan." - ".$objTemp["last_value"]."</br>";
 				return false;
 			}
 		}else{
-			//echo "ELSE2".$tabla." - ".$variable." - ".$moreThan." - ".$objTemp["last_value"]."</br>";
-			if($vals[0] > floatval($moreThan) && $vals[1] > floatval($moreThan)){
-				if($vals[0]<=$val[1]){
+			// If the last and second to last value is over the alarm value, otherway avoid the alarm
+			if($currentValue > floatval($moreThan) && $penultimateValue > floatval($moreThan)){
+				// If the last value is less than the second to last value it means it is going down, then avoid the alarm
+				if($currentValue <= $penultimateValue){
+					return false;
+					//If there is a less than value, check if the last and second to last value are below the less than value, other way avoid the alarm
+				}else if( !empty($lessThan) && $currentValue > floatval($lessThan) && $penultimateValue > floatval($lessThan) ){
 					return false;
 				}
 			}else{
@@ -126,28 +117,25 @@
 			}
 		}
 		
-		// If there is an averageTemp sum, then divide it by 2 in order to get the average
-		$average = $vals[0];//$averageTemp / $averageCount;
-
 		$measureSymbol = "";
 		// Set measure symbol
 		switch ($variable) {
-			case 'temperatura': $measureSymbol = "grados C"; 
+			case 'temperature': $measureSymbol = "grados C"; 
 								$variable = "Temperatura"; 
-								$valor=$average;  
+								$valor = $currentValue;  
 							break;
-			case 'precipitacion_real': $measureSymbol = "mm/h"; 
+			case 'realPrecipitation': $measureSymbol = "mm/h"; 
 									   $variable = "Precipitación"; 
-									   $valor = $average * 60 / 5; 
+									   $valor = $currentValue * 60 / 5; 
 									   $moreThan = $moreThan * 60 / 5; 
 									   $customMsg = "alta temperatura,"; 
 									  break;
-			case 'nivel': $measureSymbol = "cm"; 
+			case 'riverFlow': $measureSymbol = "cm"; 
 						  $variable = "Nivel"; 
-						  $valor=$average; 
+						  $valor = $currentValue; 
 						 break;
 			default: $measureSymbol = "grados C"; 
-					 $valor=$average; 
+					 $valor = $currentValue; 
 					break;
 		}
 
@@ -169,17 +157,16 @@
 			switch ($variable) {
 				case 'Temperatura': $customMsg = 'alta temperatura,'; break;
 				case 'Precipitación': $customMsg = "alta precipitación,"; break;
-				case 'Nivel': $customMsg = "altos niveles del río,";  
+				case 'Nivel': $customMsg = " alerta  ".$alarmType." de nivel del río,";  
 							  // Sets first cicle alarm value
 							  if(!$activeMoreThan){
-							  	$updatedIds[] = $station;
+							  	$updatedIds[] = $stationId;
 							  	$objTemp["first_cicle_alarm"]=$valor; 
 							  	$objTemp["last_value"] = $valor;		
 							  }
 							break;
 			}
 			
-			//echo $stationName." -> ".$variable.": valor:".$valor." > ".floatval($moreThan)."</br>";
 			$Message .= empty($msg)?"'".$stationName."' ".$customMsg."  ".$valor." ".$measureSymbol.".  ":$msg;
 			setMessage($tabla,"'".$stationName."' ".$customMsg." ".$valor." ".$measureSymbol.". ");
 			$response = true;
@@ -190,7 +177,6 @@
 
 	function setMessage($table, $ms){
 		global $specificMessage;
-
 		$specificMessage[$table] .= $ms;
 	}
 ?>
--- lib/sms_cron.php
+++ lib/sms_cron.php
@@ -16,31 +16,33 @@ include_once('sms.php');
 include_once('alarm.php');
 // Get log lib
 include_once('error.php');
-
-$Message="";
-
+// Set encoding
 mysql_query("set names 'utf8'");
 // Inicializa variables
 $Message = "";
 $specificMessage = array();
 
+$currentTime = new DateTime('', new DateTimeZone('America/Bogota'));
+$startDate = $currentTime->format("Y-m-d H:i:s");
+//echo "Start Date:".$startDate."</br>";
+
 // Current value, this is used on level last value reset
-$currentLevelValue = 0;
+//$currentLevelValue = 0;
 
 // This is used as a global variable in order to update it from checkAlarms 
 // function when its needed to set a last level value or first alarm
 $objTemp = array();
 			
 // Get alarms of estaciones
-$estacionesList = $oMySQL->executeSQL("SELECT tdps.idStation, tdps.stationName, tdps.tableName, tia.station_id, tia.alarm FROM tdp_stations AS tdps
-JOIN ti_alarm AS tia ON tia.station_id = tdps.idStation");//$oMySQL->executeSQL('SELECT * FROM tdp_stations WHERE alarma IS NOT NULL AND alarma != ""');
-
+$estacionesList = $oMySQL->executeSQL("SELECT tia.id, tdps.idStation, tdps.stationName, tdps.tableName, tia.station_id, tia.alarm 
+										FROM tdp_stations AS tdps
+										JOIN ti_alarm AS tia ON tia.station_id = tdps.idStation
+										WHERE tia.isEnable=1");// AND (tdps.idStation=40 or tdps.idStation=39) //$oMySQL->executeSQL('SELECT * FROM tdp_stations WHERE alarma IS NOT NULL AND alarma != ""');
 
 $alarmUpdateList = array();
-//var_dump($estacionesList);
 $updatedIds = array();
 // Iterates the alarms in order to send SMS		
-foreach($estacionesList as $el){
+foreach($estacionesList as $el){ 
 	// This will ensure last alarm is sent once an hour
 	$now = time();
 	// Decode alarm json in order to analyze it
@@ -62,17 +64,16 @@ foreach($estacionesList as $el){
 			$objTemp[$mathSymbol]=$symbolValue;
 			// I setup variables for less than and more than values on each variable
 			switch ($mathSymbol) {
-				case '<':$lessThan=$symbolValue;	break;
-				case '>':$moreThan=$symbolValue;  break;
-				//case 'last_sms':$last_sms = $symbolValue;  break;
+				case '<':$lessThan=$symbolValue; break;
+				case '>':$moreThan=$symbolValue; break;
 				default:break;
 			} 
 		}
-		// If the last sms for this specific variable is older than an 2 hours ago, then it check for alarms on this variable
+		// If the last sms for this specific variable is older than 2 hours ago, then it check for alarms on this variable
 		if($now > $objTemp["last_sms"]+2*60*60){
 			// Check alarm for this variable and add it to the general alarm message
 			$response = checkAlarms($oMySQL, $tabla, $stationName, $variable, $lessThan, $moreThan, $el['id']);
-			//echo $tabla." - ".$variable." - ".$moreThan." - ".$objTemp["last_value"]."</br>";
+			//echo  "tabla: ".$tabla." - stationName: ".$stationName." - variable: ".$variable." - lessThan: ".$lessThan." - moreThan: ".$moreThan." - id: ".$el['id']."</br>";
 			// If there were any alarm detected for this variable then update the last sms variable of the json for the specific variable
 			if($response){
 				$updatedIds[] = $el['id'];
@@ -98,12 +99,14 @@ if(!empty($updatedIds)){
 	}	
 }
 
-
 // TEST CODE
-//var_dump($alarmUpdateList);
-//echo "</br></br>";
-//var_dump($Message);
-/*if(!empty($Message)){
+/*echo "</br></br>AlarmUpdateList: ";
+var_dump($alarmUpdateList);
+echo "</br></br>";*/
+/*
+if(!empty($Message)){
+	//echo "</br></br>Message: ";
+	//var_dump($Message);
 	// Make sure we have the right timezone
 	$dt = new DateTime('', new DateTimeZone('America/Bogota'));
 	// Set message format with the date of the sms
@@ -115,25 +118,31 @@ if(!empty($updatedIds)){
 	//$updateSMS = $oMySQL->executeSQL('UPDATE sms SET messages = messages-'.$totalSentSMS.' WHERE id=2');
 	//AltiriaSMS("573136355940", $msg, "dvargas", false);
 	@error_log(PHP_EOL.PHP_EOL."TEST CODE: ".$msg.PHP_EOL, 3, "/home/thinkclo/public_html/redh/sms.log");
-	echo "TEST CODE:".$tab.": ".$msg."</br>";
+	echo $msg;
 	
 }else{
 	echo "There is no alarm to be sent";
-}	
-exit();*/
+}
+$currentTime = new DateTime('', new DateTimeZone('America/Bogota'));
+$endDate = $currentTime->format("Y-m-d H:i:s");
+echo "</br>End Date:".$endDate."</br>";
+//$diff = $endDate->diff($startDate); //date_diff($startDate,$endDate);
+echo "</br>Time: ".$endDate->diff($startDate)."</br>";
+exit();
 // END TEST CODE
+*/
 
+// Make sure we have the right timezone
+$dt = new DateTime('', new DateTimeZone('America/Bogota'));
+	
 // I check if there is a pending to send message
 if(!empty($Message)){
-	// Make sure we have the right timezone
-	$dt = new DateTime('', new DateTimeZone('America/Bogota'));
 	// Set message format with the date of the sms
 	$msg = $dt->format("Y-m-d H:i:s")." Alarma: ".$Message."Visítanos redhidro.org";
 	// Get all the amount of remaining messages from the vendor
 	$smsCount = $oMySQL->executeSQL('SELECT messages FROM ti_sms WHERE id = 2');	
 	// if I have more than 50 sms
-	if(intval($smsCount["messages"])>50){
-		//Old contacts: $allStations = "573113858761,573137466206,573148216816,573128949483,573206310432,573163461538,573163609097,573223195922,573103545446,573148141462,573136355940,573116237381,573148331642,573206770012,573177342310,573163000496";
+	if(intval($smsCount["messages"])>=30){
 		$allStations = "573137466206,573148216816,573128949483,
 						573206310432,573163461538,573163609097,
 						573223195922,573117875490,573148141462,
@@ -201,7 +210,7 @@ if(!empty($Message)){
 		@error_log(PHP_EOL.PHP_EOL.$totalMessages."->".$smsCount["messages"].":".$totalRemainMessages, 3, "/home/thinkclo/public_html/redh/sms.log");
 		// Return response on manual cron run
 		echo $totalMessages."->".$smsCount["messages"].":".$totalRemainMessages."</br>";
-	}else if(intval($smsCount["messages"])<=50){
+	}else if(intval($smsCount["messages"])<30){
 		// Send out of messages warning
 		$outOfMessagesMsg = $dt->format("Y-m-d H:i:s")." Alarma: ".$Message.". Sus mensajes se están agotando, solo le quedan xx, comuníquese con Think Cloud Group para comprar un paquete adicional.";
 
@@ -214,7 +223,7 @@ if(!empty($Message)){
 		$outOfMessagesMsg = $dt->format("Y-m-d H:i:s")." Alarma: ".$Message.". Sus mensajes se están agotando, solo le quedan ".$totalRemainMessages.", comuníquese con Think Cloud Group para comprar un paquete adicional.";	
 		
 		
-		$outOfMessages = "573136355940,573234335384";//573108311240
+		$outOfMessages = "573136355940,573234335384,573108311240";
 		$updateSMS = $oMySQL->executeSQL('UPDATE ti_sms SET messages = '.$totalRemainMessages.' WHERE id=2');
 		$resp["outofmessages"] = AltiriaSMS($outOfMessages, $outOfMessagesMsg, "dvargas", false);
 		@error_log(PHP_EOL.PHP_EOL."OUT OF MESSAGES: ".$outOfMessagesMsg.PHP_EOL.$resp["outofmessages"], 3, "/home/thinkclo/public_html/redh/sms.log");
@@ -226,6 +235,8 @@ if(!empty($Message)){
 		echo $response."</br></br>";
 	}
 }else{
-	echo "There is no alarm to be sent";
+	//$response = $dt->format("Y-m-d H:i:s")." No hay alarmas pendientes para enviar. Visítanos redhidro.org";
+	//error_log(PHP_EOL.PHP_EOL.$response, 3, "/home/thinkclo/public_html/redh/sms.log");
+	echo "</br>There is no alarm to be sent</br>";
 }
 ?>
